673f0742e18da68ec11a29a2c64160caa2942593
Your Name <you@example.com>
2026-07-21 02:05:27 -0400

    added fillingArray.py

---
+++ Chapter7Notebook/fillingArray.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+twoByThreeArray = np.array([[1,1,1],[1,1,1]])
+print(twoByThreeArray)
+
+threeByThreeArray = np.array([[0,0,0], [0,0,0], [0,0,0]])
+print(threeByThreeArray)
+
+twoByFiveArray = np.array([[7,7,7,7,7], [7,7,7,7,7]])
+print(twoByFiveArray)
