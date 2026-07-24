1512aa7bfba0793f169ee2ccf6c030c04c68c37d
Your Name <you@example.com>
2026-07-24 03:27:35 -0400

    convertIntegerToEmoji.py

---
+++ summer2026python/Chapter8Notebook/convertIntegerToEmoji.py
@@ -0,0 +1,15 @@
+emojiDictonary = {
+    "Smile": 0x1F601,
+    "Laugh": 0x1F602,
+    "UpsideDownFace": 0x1F643,
+    "HaloFace": 0x1F607,
+    "BlowingKissFace": 0x1F618,
+    "MoneyFace": 0x1F911,
+    "ZippedFace": 0x1F910,
+    "NoExpressionFace": 0x1F611,
+    "SleepyFace": 0x1F634,
+    "VomitFace": 0x1F92E
+}
+
+for key in emojiDictonary.keys():
+    print(chr(emojiDictonary[key]))
